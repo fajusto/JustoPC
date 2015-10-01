da5e2f3fb8041158f33db4b29110cd1716c2a29c
Fabrício Justo <fajusto@icloud.com>
2015-10-01 14:19:21 -0300

    Mais funções lição 1.0

--- mad.php
+++ mad.php
@@ -46,7 +46,7 @@ echo "<br>";
 
 //unlink (apaga arquivos)
 
-$arquivo = "/opt/lampp/htdocs/php_studies/texto";
+/*$arquivo = "/opt/lampp/htdocs/php_studies/texto";
 if (unlink($arquivo))
 {
 	echo "Arquivo apagado";
@@ -54,6 +54,79 @@ if (unlink($arquivo))
 else
 {
 	echo "Arquivo não apagado";
+}
+
+echo "<br>";*/
+
+//file_exists (verifica a existência de um arquivo)
+
+$arquivo = '/opt/lampp/htdocs/php_studies/index.php';
+if (file_exists($arquivo))
+{
+	echo "Arquivo existente<br>";
+}
+else
+{
+	echo "Arquivo inexistente<br>";
+}
+
+echo "<br>";
+
+//is_file (verifica se a localização corresponde ou não à um arquivo)
+
+$arquivo = "/home/fabricio/Imagens/matrix_numbers_code_background_hogh_contrast_hd-wallpaper-45565.jpg";
+if (is_file($arquivo))
+{
+	echo "$arquivo é um arquivo<br>";
+}
+else
+{
+	echo "$arquivo não é um arquivo<br>";
+}
+
+echo "<br>";
+
+//mkdir (Cria um diretório)
+
+/*$dir = "/opt/lampp/htdocs/php_studies/Novas Imagens";
+if (mkdir($dir, 0777))
+{
+	echo "$dir Criado com sucesso<br>";
+}
+else
+{
+	echo "$dir Não criado<br>";
+}*/
+
+echo "<br>";
+
+//getcwd (Retorna o diretório corrente)
+
+echo "o diretório corrente é " . getcwd();
+echo "<br>";
+
+echo "<br>";
+
+//chdir (Altera o diretório corrente. Retorna TRUE em caso de sucesso e False am caso de falhas)
+
+echo "o diretório atual é " . getcwd();
+echo "<br>";
+chdir("/opt/lampp/htdocs/php_studies");
+echo "o diretório atual é " . getcwd();
+echo "<br>";
+
+echo "<br>";
+
+//rmdir (Apaga um diretório)
+
+$dir = "/opt/lampp/htdocs/php_studies/Novas Imagens";
+if (rmdir($dir))
+{
+	echo "$dir Apagado com sucesso!<br>";
+}
+else
+{
+	echo "$dir Não apagado.<br>";
 }
 
 echo "<br>";
