34eac2f52cb75b88d5ed7d011af1a63c60db8b34
Fabrício Justo <fajusto@icloud.com>
2015-10-02 16:03:40 -0300

    Documento classes.php adicionado

---
+++ classes.php
@@ -0,0 +1,76 @@
+<?php
+//2.2 Manipulação de Objetos
+class Pessoa
+{
+	var $Codigo;
+	var $Nome;
+	var $Altura;
+	var $Idade;
+	var $Nascimento;
+	var $Escolaridade;
+	var $Salario;
+	
+	{
+		if ($centimetros > 0)
+		{
+			$this->Altura += $centimetros;
+		}
+	}
+
+
+	function Formar($titulaçao)
+	{
+		$this->Escolaridade = $titulaçao;
+	}
+
+	function Envelhecer($anos)
+	{
+		if ($anos > 0)
+		{
+			$this->Idade += $anos;
+		}
+	}
+}
+
+class Conta
+{
+	var $Agencia;
+	var $Codigo;
+	var $DataDeCriacao;
+	var $Titular;
+	var $Senha;
+	var $Saldo;
+	var $Cancelada;
+
+	function Retirar($quantia)
+	{
+		if ($quantia > 0)
+		{
+			$this->Saldo -= $quantia;
+		}
+	}
+	function Depositar($quantia)
+	{
+		if ($quantia > 0)
+		{
+			$this->Saldo += $quantia;
+		}
+	}
+	function ObterSaldo()
+	{
+		return $this->Saldo;
+	}
+}
+
+include_once 'classes/Pessoa.class.php';
+include_once 'classes/Conta.class.php';
+
+$carlos = new Pessoa;
+$carlos->Codigo = 10;
+$carlos->Nome = "Carlos da Silva";
+$carlos->Altura = 1.85;
+$carlos->Idade = 25;
+$carlos->Nascimento = '10/04/1976';
+$carlos->Escolaridade = "Ensino Médio";
+
+echo "Manipulando o objeto $carlos-Nome :\n";
