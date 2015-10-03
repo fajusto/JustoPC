37cb7521f320f6ba7ea6ff417d4dcb82f7f12b8b
Fabrício Justo <fajusto@icloud.com>
2015-10-02 23:27:32 -0300

    Classe completada

--- classes.php
+++ classes.php
@@ -12,5 +12,34 @@ $carlos->Idade = 25;
 $carlos->Nascimento = '10/04/1976';
 $carlos->Escolaridade = "Ensino Médio";
 
-echo "Manipulando o objeto $carlos->Nome :\n";
+echo "Manipulando o objeto $carlos->Nome :\n<br>";
 
+echo "{$carlos->Nome} é formado em: {$carlos->Escolaridade} \n<br>";
+$carlos->Formar('Técnico em Eletricidade');
+echo "{$carlos->Nome} é formado em: {$carlos->Escolaridade}\n<br>";
+
+echo "{$carlos->Nome} possui {$carlos->Idade} anos \n<br>";
+$carlos->Envelhecer(1);
+echo "{$carlos->Nome} possui {$carlos->Idade} anos \n<br>";
+
+
+//Conta Carlos
+
+$conta_carlos = new Conta;
+$conta_carlos->Agencia = 6677;
+$conta_carlos->Codigo = "CC.1234.56";
+$conta_carlos->DataDeCriacao = "10/07/02";
+$conta_carlos->Titular = $carlos;
+$conta_carlos->Senha = 9876;
+$conta_carlos->Saldo = 567.89;
+$conta_carlos->Cancelada = false;
+
+echo "\n";
+echo "Manipulado a conta de: {$conta_carlos->Titular->Nome} \n<br>";
+echo "O saldo atual é R\$ {$conta_carlos->ObterSaldo()} \n<br>";
+
+$conta_carlos->Depositar(20);
+echo "O saldo atual é R\$ {$conta_carlos->ObterSaldo()} \n<br>";
+
+$conta_carlos->Retirar(10);
+echo "O saldo atual é R\$ {$conta_carlos->ObterSaldo()} \n<br>";
